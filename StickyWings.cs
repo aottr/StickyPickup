4d1d42687931cb8d8a2dab35ae1e738441b8d28f
StaubRein <me@staubrein.com>
2021-12-07 10:29:51 +0100

    introduce wings

---
+++ StickyWings.cs
@@ -0,0 +1,18 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using VRC.SDK3.Components;
+
+public class StickyWings : UdonSharpBehaviour
+{
+    public VRCObjectPool ObjectPool;
+
+    public override void Interact()
+    {
+        var obj = ObjectPool.TryToSpawn();
+        obj.transform.position = Networking.LocalPlayer.GetBonePosition(HumanBodyBones.Chest) - new Vector3(0,0,0.3f);
+        obj.transform.rotation = Networking.LocalPlayer.GetRotation();
+    }
+}
